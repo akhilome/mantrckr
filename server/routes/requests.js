1fc6a89ebb5d4afe2afa3a46209f0e181fcaefe5
akhilome <kizito@akhilo.me>
2018-05-24 00:51:03 +0100

    feat(api): create get single request route
    - adds api route to get single request
    
    [Finishes #157740786]

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -8,5 +8,19 @@ router.get('/requests', (req, res) => {
   res.status(200).json({ userRequests });
 });
 
+// Get single request
+router.get('/requests/:requestId', (req, res) => {
+  let reqId = req.params.requestId;
+  reqId = Number(reqId);
+  if (Number.isNaN(reqId)) {
+    return res.status(400).json({ message: 'Request ID must be a number' });
+  }
+  const result = userRequests.find(request => request.requestId === reqId);
+
+  if (!result) {
+    return res.status(404).json({ message: 'No request with that Id exists' });
+  }
+  return res.status(200).json({ result });
+});
 
 export default router;
