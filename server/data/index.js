29594a0cec1d545f5b8a46451ef3712a7e38c00c
akhilome <kizito@akhilo.me>
2018-05-23 23:42:45 +0100

    chore(update data): update dummy data
    [Finishes #157828102]

--- server/data/index.js
+++ server/data/index.js
@@ -1,28 +1,22 @@
-const userRequests = [
+export const users = [];
+
+export const userRequests = [
   {
-    userId: 1,
     requestId: 1,
-    userDept: 'accounting',
     requestTitle: 'Faulty counting machine',
     requestDetails: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     status: 'done',
   },
   {
-    userId: 2,
     requestId: 2,
-    userDept: 'engineering',
     requestTitle: 'Faulty bench press',
     requestDetails: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     status: 'pending',
   },
   {
-    userId: 3,
     requestId: 3,
-    userDept: 'hr',
     requestTitle: 'Faulty bench press',
     requestDetails: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     status: 'rejected',
   },
 ];
-
-export default userRequests;
