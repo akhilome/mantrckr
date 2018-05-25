91ed7c820b4eac0bb0097b2b86a8885c0745f15e
akhilome <kizito@akhilo.me>
2018-05-25 02:33:16 +0100

    fix(coveralls): implement fix 4
    - fix eaddrinuse error when running test

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import app from './server/index';
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`App is listening at port ${PORT}`);
-});
+if (!module.parent) {
+  app.listen(PORT, () => {
+    console.log(`App is listening at port ${PORT}`);
+  });
+}
 
 export default app;
