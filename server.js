b893c0a2637e9cf93f900e8e5705311d71bf3554
akhilome <kizito@akhilo.me>
2018-05-25 03:29:04 +0100

    chore(coveralls): implement fix 5

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 import app from './server/index';
 
 const PORT = process.env.PORT || 3000;
-if (!module.parent) {
-  app.listen(PORT, () => {
-    console.log(`App is listening at port ${PORT}`);
-  });
-}
+if (!module.parent) app.listen(PORT);
 
 export default app;
